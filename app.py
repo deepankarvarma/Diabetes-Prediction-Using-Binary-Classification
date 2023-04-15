e6208e132fd416793da297ace7bdd83ba3a22b70
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-15 18:23:58 +0530

    final working code

---
+++ app.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.linear_model import RidgeClassifier
+import streamlit as st
+
+# Load the CSV file into a pandas dataframe
+st.set_page_config(page_title="Diabetes Prediction")
+df = pd.read_csv('diabetes2.csv')
+st.title("Diabetes Prediction Using Regression")
+st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://wallpaperaccess.com/full/1567677.jpg");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+# Accept user inputs for the feature columns
+pregnancies = st.number_input('Pregnancies',step=1)
+glucose = st.number_input('Glucose')
+blood_pressure = st.number_input('Blood Pressure')
+skin_thickness = st.number_input('Skin Thickness')
+insulin = st.number_input('Insulin')
+bmi = st.number_input('BMI')
+dpf = st.number_input('Diabetes Pedigree Function')
+age = st.number_input('Age',step=1)
+
+# Use the model to make predictions on the user inputs
+model = RidgeClassifier()
+model.fit(df.drop('Outcome', axis=1), df['Outcome'])
+prediction = model.predict([[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, dpf, age]])
+
+# Display the prediction to the user
+if prediction[0] == 0:
+    st.write('Prediction: Not Diabetic')
+else:
+    st.write('Prediction: Diabetic')
---
+++ model.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import RidgeClassifier
+from sklearn.metrics import classification_report
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
+# Load the CSV file into a pandas dataframe
+df = pd.read_csv('diabetes2.csv')
+
+# Making a count vs outcount plot to check the balance in data 
+sns.catplot(x="Outcome", kind="count", data=df, palette="Set2")
+plt.show()
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(df.drop('Outcome', axis=1), df['Outcome'], test_size=0.2, random_state=42)
+
+# Create a linear regression model and fit it to the training data
+model = RidgeClassifier()
+model.fit(X_train, y_train)
+
+# Use the model to make predictions on the testing data
+y_pred = model.predict(X_test)
+
+# Evaluate the accuracy of the model
+Classification_report = classification_report(y_test, y_pred)
+print(f"Classification Report:\n {Classification_report}")
+
+data = [[5, 150, 33.7, 50, 150, 74, 0.5, 53]]
+
+# Create the pandas DataFrame 
+df_test = pd.DataFrame(data, columns = ['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age'])
+
+# Predict on new data
+res = model.predict(df_test)
+print(res)
